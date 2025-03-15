089f61c86997c25085bc228ffcce19068cfebb94
JuanDavidKL <juand.castanedaa@KonradLorenz.edu.co>
2025-03-15 14:26:37 -0500

    Update Nodo.java
    edad agregada

--- Nodo.java
+++ Nodo.java
@@ -5,12 +5,14 @@ public class Nodo {
 //Creación de los atributos principales del objeto “Paciente”, dichos objetos serán almacenados en las celdas de los nodos correspondientes
     private String nombre;
     private int cedula;
+    private int edad;
     private Nodo Next;
 
 //A continuación, se generan los “get” y “set” que nos permitirán manipular y trabajar con la información de los objetos “Paciente” almacenados en sus respectivos nodos
-    public Nodo(String nombre, int cedula, Nodo Next) {
+    public Nodo(String nombre, int cedula, int edad, Nodo Next) {
         this.nombre = nombre;
         this.cedula = cedula;
+        this.edad = edad;
         this.Next = Next;
     }
 
@@ -20,6 +22,10 @@ public class Nodo {
 
     public int getCedula() {
         return cedula;
+    }
+
+    public int getEdad() {
+        return edad;
     }
 
     public Nodo getSig() {
@@ -32,6 +38,10 @@ public class Nodo {
 
     public void setCedula(int cedula) {
         this.cedula = cedula;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 
     public void setNext(Nodo sig) {
