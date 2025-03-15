87675dbb354e723fa0c38ac78542622681db4e8b
JuanDavidKL <juand.castanedaa@KonradLorenz.edu.co>
2025-03-15 15:18:50 -0500

    Update Consultorio.java

--- Consultorio.java
+++ Consultorio.java
@@ -1,7 +1,11 @@
 package consultorio;
 
+import java.util.Scanner;
+
 public class Consultorio {
 
     public static void main(String[] args) {
+        Scanner Tec =  new Scanner(System.in);
+        Lista_Consultas Consultas_de_hoy = new Lista_Consultas();
     }
 }
