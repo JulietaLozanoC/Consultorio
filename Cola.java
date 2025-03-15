f5c0f48c78d7437f1f9d0eacb2eb7b6a8dfffecf
JuanDavidKL <juand.castanedaa@KonradLorenz.edu.co>
2025-03-15 15:19:24 -0500

    Update Cola.java

--- Cola.java
+++ Cola.java
@@ -11,5 +11,16 @@ public class Cola {
       /*Método que nos permitirá ingresar la información de los nuevos “Pacientes” al final
 de la lista simplemente enlazada para así retomarla en el orden que va entrando (el
 primer dato en entrar es el primero en salir)*/
-
+    public void Insertar_Nuevo_Nodo(String nombre, int cedula, int edad) {
+        Nodo nuevo = new Nodo(nombre, cedula, edad, null);
+        if (primero == null) {
+            primero = nuevo;
+        } else {
+            Nodo actual = primero;
+            while (actual.getSig() != null) {
+                actual = actual.getSig();
+            }
+            actual.setNext(nuevo);
+        }
+    }
 }
