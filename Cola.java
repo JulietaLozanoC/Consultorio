238ef55ecd15c4305c89bbcdbd277d4b36a5782b
JuanDavidKL <juand.castanedaa@KonradLorenz.edu.co>
2025-03-15 15:09:33 -0500

    Update Cola.java

--- Cola.java
+++ Cola.java
@@ -1,3 +1,15 @@
 package consultorio;
 
-public class Cola {}
+public class Cola {
+  
+    private Nodo primero;
+
+    public Lista_Consultas() {
+        primero = null;
+    }
+
+      /*Método que nos permitirá ingresar la información de los nuevos “Pacientes” al final
+de la lista simplemente enlazada para así retomarla en el orden que va entrando (el
+primer dato en entrar es el primero en salir)*/
+
+}
